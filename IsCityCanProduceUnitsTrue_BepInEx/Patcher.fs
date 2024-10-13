8fe5e72291a1934459a902c0a0f85b5704bb5d80
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-13 14:58:53 +0300

    New mod added

---
+++ IsCityCanProduceUnitsTrue_BepInEx/Patcher.fs
@@ -0,0 +1,27 @@
+﻿namespace IsCityCanProduceUnitsTrue_BepInEx
+
+open Mono.Cecil
+open Mono.Cecil.Cil
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+
+module Patcher =
+
+    let targetDLLs: IEnumerable<string> = [ "Assembly-CSharp.dll" ]
+
+    let Initialize () = ()
+
+    let Finish () = ()
+
+    let Patch (assembly: AssemblyDefinition) =
+        let mainModule = assembly.MainModule
+
+        let CityBehProduceUnit = mainModule.Types |> Seq.find (fun t -> t.Name = "CityBehProduceUnit")
+        let isCityCanProduceUnits = CityBehProduceUnit.Methods |> Seq.find (fun m -> m.Name = "isCityCanProduceUnits")
+        let isCityCanProduceUnitsIL = isCityCanProduceUnits.Body.GetILProcessor()
+
+        for _ in 1..6 do
+            isCityCanProduceUnitsIL.Remove(isCityCanProduceUnits.Body.Instructions.[0])
+
---
+++ IsCityCanProduceUnitsTrue_BepInEx/Script.fsx
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+#load "Patcher.fs"
+open IsCityCanProduceUnitsTrue_BepInEx
+
+// Define your library scripting code here
+
